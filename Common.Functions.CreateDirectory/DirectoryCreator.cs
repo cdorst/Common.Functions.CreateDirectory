8de1f556d98dcb41c6287c30ec199256a05f386a
Christopher Dorst <chris@dorst.email>
2017-12-25 16:11:45 -0600

    Adding DirectoryCreator

---
+++ Common.Functions.CreateDirectory/DirectoryCreator.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace Common.Functions.CreateDirectory
+{
+    public static class DirectoryCreator
+    {
+        public static void Create(string path) => Directory.CreateDirectory(path);
+
+        public static void CreateSubdirectories(string pathRelativeToCurrent)
+            => Create(GetFullPath(pathRelativeToCurrent));
+
+        private static string GetFullPath(string pathRelativeToCurrent)
+            => Path.Combine(Directory.GetCurrentDirectory(), pathRelativeToCurrent);
+    }
+}
